b03a1df5b09a6087272b77727b7c88419b0fe9a9
freeloop <freeloop@gmail.com>
2022-11-21 15:02:40 +0800

    bus+rabbitMQ动态刷新全局广播配置

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,7 @@
+package com.atguigu.springcloud;/**
+ * ConfigClientMain3366
+ * 
+ * @author fj
+ * @date 2022/11/21 14:22
+ */public class ConfigClientMain3366 {
+}
---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,7 @@
+package com.atguigu.springcloud.controller;/**
+ * ConfigClientController
+ * 
+ * @author fj
+ * @date 2022/11/21 14:23
+ */public class ConfigClientController {
+}
