9da90b320571cef7c89acb7b0f985e392347fec1
freeloop <freeloop@gmail.com>
2022-11-22 23:18:13 +0800

    nacos服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,7 @@
+package com.atguigu.springcloud;/**
+ * NacosConfigClientMain3377
+ * 
+ * @author fj
+ * @date 2022/11/22 20:58
+ */public class NacosConfigClientMain3377 {
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,7 @@
+package com.atguigu.springcloud.controller;/**
+ * ConfigClientController
+ * 
+ * @author fj
+ * @date 2022/11/22 21:01
+ */public class ConfigClientController {
+}
