f73e1ea5f533aadbd49c3f99484b1b9821b636ef
freeloop <freeloop@gmail.com>
2022-11-24 21:04:22 +0800

    SentinelResource

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
@@ -0,0 +1,7 @@
+package com.atguigu.springcloud.controller;/**
+ * RateLimitController
+ * 
+ * @author fj
+ * @date 2022/11/24 19:36
+ */public class RateLimitController {
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/myhandler/CustomerBlockHandler.java
@@ -0,0 +1,7 @@
+package com.atguigu.springcloud.myhandler;/**
+ * CustomerBlockHandler
+ * 
+ * @author fj
+ * @date 2022/11/24 19:48
+ */public class CustomerBlockHandler {
+}
